f45fd0c82f0bd38ba196b48f5075ce1bdea96310
Hunter Owens <owens.hunter@gmail.com>
2013-10-31 16:06:41 -0500

    find articles

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import psycopg2
 import os
 
@@ -10,12 +10,19 @@ DB_USER = os.environ.get('WIKI_USER')
 DB_DB = os.environ.get('WIKI_DB')
 DB_PW = os.environ.get('WIKI_PW')
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def std():
 	conn = psycopg2.connect('host=%s dbname=%s user=%s password=%s' % (DB_HOST,DB_DB,DB_USER,DB_PW))
-	cursor = conn.cursor()
-	return render_template('index.html')
-
+	cur = conn.cursor()
+	if request.method == 'POST':
+		print test
+	elif request.method == 'GET':
+		cur.execute("SELECT * from articles order by random() limit 1")
+		res = cur.fetchall()
+		print res
+		article = res[0]
+		return render_template('index.html',article=article)
+		
 if __name__=="__main__":
 	app.debug = True
 	app.run()
