1363545cf9f7c8ffc4102605b16623307cd4bbc7
Hunter Owens <owens.hunter@gmail.com>
2013-10-31 15:26:12 -0500

    db and flask

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import flask
-from flask import Flask
+from flask import Flask, render_template
+import psycopg2
+import os
 
 app = Flask(__name__)
 
+DB_HOST = os.environ.get('WIKI_DB_HOST')
+DB_USER = os.environ.get('WIKI_USER')
+DB_DB = os.environ.get('WIKI_DB')
+DB_PW = os.environ.get('WIKI_PW')
+
 @app.route('/')
 def std():
-	return 'hello world'
+	conn = psycopg2.connect('host=%s dbname=%s user=%s password=%s' % (DB_HOST,DB_DB,DB_USER,DB_PW))
+	cursor = conn.cursor()
+	return render_template('index.html')
 
 if __name__=="__main__":
+	app.debug = True
 	app.run()
