4beac8cf71c2ce9364b1175776f4b6cf3d768515
Hunter Owens <owens.hunter@gmail.com>
2013-11-17 19:44:35 -0600

    changing the tracker  to psudcode

--- wikipedia_tracker.py
+++ wikipedia_tracker.py
@@ -1,1 +1,23 @@
-import wikipedia 
+import requests
+
+for result in query( {'generator':'allpages', 'prop':'links'} ):
+    # process result data
+    print result
+
+def query(request):
+    request['action'] = 'query'
+    request['format'] = 'json'
+    lastContinue = dict()
+    while True:
+        # Clone original request
+        req = request.copy()
+        # Modify it with the values returned in the 'continue' section of the last result.
+        req.update(lastContinue)
+        # Call API
+        result = requests.get('http://en.wikipedia.org/w/api.php', params=req).json()
+        if 'error' in result: raise Error(result['error'])
+        if 'warnings' in result: print(result['warnings'])
+        if 'query' in result: yield result['query']
+        if 'continue' not in result: break
+        lastContinue = result['continue']
+ 
